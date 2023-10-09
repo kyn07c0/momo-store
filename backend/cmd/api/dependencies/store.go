88f4b129b5fd7aa5a7fb3b15bdcb7107027436a8
std-017-006 <kyn07c0@yandex.ru>
2023-10-09 19:40:49 +0000

    Delete images to s3

--- backend/cmd/api/dependencies/store.go
+++ backend/cmd/api/dependencies/store.go
@@ -1,8 +1,8 @@
 package dependencies
 
 import (
-	"gitlab.praktikum-services.ru/std-017-006/momo-store/internal/store/dumplings"
-	"gitlab.praktikum-services.ru/std-017-006/momo-store/internal/store/dumplings/fake"
+	"gitlab.praktikum-services.ru/Stasyan/momo-store/internal/store/dumplings"
+	"gitlab.praktikum-services.ru/Stasyan/momo-store/internal/store/dumplings/fake"
 )
 
 // NewFakeDumplingsStore returns new fake store for app
