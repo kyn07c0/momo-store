12f0def243b4633e52c512c0bea850c71c9a14d9
kyn07c0 <50650263+kyn07c0@users.noreply.github.com>
2023-10-19 14:26:01 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,8 +4,8 @@ terraform {
   bucket = "kyn07c0-terraform"
   region = "ru-central1"
   key = "kyn07c0-terraform/terraform.tfstate"
-  access_key = "YCAJEfjNgoAiUjfrv6Xg7iFFu"
-  secret_key = "YCOFq_EqgsAo10yGdRxvZUwqFmuMkbhRDkTQE2z9" 
+  access_key = ""
+  secret_key = "" 
   skip_region_validation = true
   skip_credentials_validation = true
   }
